2f91ff9fff158840d3f112c133f91d1a23d989d5
suhanijpatel <sjp269@cornell.edu>
2024-11-05 12:49:51 -0500

    cleaned personal income data

---
+++ data/clean_personal_income.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+file_path = 'personal-income-data.csv'
+data = pd.read_csv(file_path)
+
+personal_income_data = data[data['Description'].str.contains('Personal income', case=False, na=False)]
+
+columns_to_keep = ['GeoFIPS', 'GeoName', 'Region', 'Description'] + [str(year) for year in range(2000, 2024)]
+filtered_data = personal_income_data[columns_to_keep]
+
+filtered_data.to_csv('cleaned-personal-income.csv', index=False)
+

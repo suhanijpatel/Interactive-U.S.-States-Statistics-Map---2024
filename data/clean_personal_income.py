02fb30ecb71154f901b1049786f66bfc47982283
Rhea Verma <rv245@cornell.edu>
2024-11-11 22:03:57 -0500

    updated project

--- data/clean_personal_income.py
+++ data/clean_personal_income.py
@@ -3,7 +3,7 @@ import pandas as pd
 file_path = 'personal-income-data.csv'
 data = pd.read_csv(file_path)
 
-personal_income_data = data[data['Description'].str.contains('Personal income', case=False, na=False)]
+personal_income_data = data[data['Description'].str.contains('Personal income (millions of dollars)', case=False, na=False, regex=False)]
 
 columns_to_keep = ['GeoFIPS', 'GeoName', 'Region', 'Description'] + [str(year) for year in range(2000, 2024)]
 filtered_data = personal_income_data[columns_to_keep]

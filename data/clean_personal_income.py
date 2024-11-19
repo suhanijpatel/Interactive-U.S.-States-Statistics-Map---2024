d5dc88d33c611fb959a2eedb25c5ecddd207fb93
Rhea Verma <rv245@cornell.edu>
2024-11-18 20:45:26 -0500

    added comments to python scripts

--- data/clean_personal_income.py
+++ data/clean_personal_income.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
+#file path for personal income data CSV
 file_path = 'personal-income-data.csv'
+#read file into DataFrame
 data = pd.read_csv(file_path)
 
+#filter data to only include rows with relevant Description value
 personal_income_data = data[data['Description'].str.contains('Personal income (millions of dollars)', case=False, na=False, regex=False)]
 
+#keeping columns with following identifiers for years 2003 - 2023
 columns_to_keep = ['GeoFIPS', 'GeoName', 'Region', 'Description'] + [str(year) for year in range(2000, 2024)]
+
+#filter previously filtered rows to only include columns we want
 filtered_data = personal_income_data[columns_to_keep]
 
+#write to new file
 filtered_data.to_csv('cleaned-personal-income.csv', index=False)
 
--- data/gdp_cleaning.py
+++ data/gdp_cleaning.py
@@ -1,15 +1,17 @@
-import json
 import pandas as pd
+
+#file path for GDP data CSV
 file_path = 'summary_bls_data.csv'
 data = pd.read_csv(file_path)
 
+#filter data to only include rows with relevant Description value
 gdp_data = data[data['Description'].str.contains('Gross domestic product', case=False, na=False)]
-employment_data=data[data['Description'].str.contains('Gross domestic product', case=False, na=False)]
+
+#keeping columns with following identifiers for years 2003 - 2023
 columns_to_keep = ['GeoFIPS', 'GeoName', 'Region', 'Description', ] + [str(year) for year in range(2000, 2024)]
 
+#filter previously filtered rows to only include columns we want
 filtered_gdp_data = gdp_data[columns_to_keep]
-filtered_employment_data = employment_data[columns_to_keep]
 
+#write to new file
 filtered_gdp_data.to_csv('cleaned-gdp.csv', index=False)
-filtered_employment_data.to_csv= employment_data[columns_to_keep]
-
